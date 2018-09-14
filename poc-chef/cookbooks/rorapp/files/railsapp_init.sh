81130be51ef9f3fd809d05a3a8d03a3ac6a3ed9f
root <root@pt-teltmm16747.persistent.co.in>
2018-09-14 13:55:45 +0530

    fix mising file

---
+++ poc-chef/cookbooks/rorapp/files/railsapp_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# script to init the ruby application
+
+initialize() {
+if [ -d /var/www/railsapp/project_management_demo ]; then
+  cd /var/www/railsapp/project_management_demo && \
+  nohup rails server -b 0.0.0.0 &
+else
+  echo -e "application dir not found"
+fi
+}
+
+if [ "$(whoami)" = "root" ]; then
+  echo -e "initializing rails app"
+  initialize| tee -a /var/log/railsapp.log
+else
+  echo -e "you were not root"
+fi
